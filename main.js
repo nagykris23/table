13f8250bbd67567c671de947cbfbb1b038b2ba3d
Nagy Kristof <nagy.kristof@BOLYAI.LOCAL>
2024-10-04 15:12:05 +0200

    added extended person

--- main.js
+++ main.js
@@ -1,45 +1,59 @@
-const array = [
+let array = [
     {
         firstname1: 'Géza',
         firstname2: 'Ferenc',
-        lastname: 'Kocsis'
+        lastname: 'Kocsis',
+        married: true,
+        pet: 'kutya'
     },
     {
         firstname1: 'Mária',
         firstname2: 'Júlia',
-        lastname: 'Horváth'
+        lastname: 'Horváth',
+        married: false,
+        pet: 'macska'
     },
     {
         firstname1: 'Ferenc',
-        lastname: 'Balogh'
+        lastname: 'Balogh',
+        married: false,
+        pet: 'teknős'
     },
     {
         firstname1: 'Gábor',
         firstname2: 'Attila',
-        lastname: 'Horváth'
+        lastname: 'Horváth',
+        married: true,
+        pet: 'macska'
     },
 ]
- 
- 
+
+
 const table = document.createElement("table");
 const thead = document.createElement("thead");
 const tr = document.createElement("tr");
 const th_lastname = document.createElement("th");
 const th_firstname = document.createElement("th");
+const th_hazas = document.createElement("th");
+const th_pet = document.createElement("th");
 const tbody = document.createElement("tbody");
- 
+
 document.body.appendChild(table);
 table.appendChild(thead);
 thead.appendChild(tr);
 tr.appendChild(th_lastname);
 tr.appendChild(th_firstname);
+tr.appendChild(th_hazas);
+tr.appendChild(th_pet);
 table.appendChild(tbody);
- 
+
 th_lastname.innerHTML = "Vezeténév";
 th_firstname.innerHTML = "Keresztnév";
+th_hazas.innerHTML = "Házas";
+th_pet.innerHTML = "Pet"
 th_firstname.colSpan = 2;
- 
-for(let pers of array){
+
+for (let pers of array) {
     const tr_body = document.createElement("tr");
     tbody.appendChild(tr_body);
     const td_lastname = document.createElement("td");
@@ -48,13 +62,36 @@ for(let pers of array){
     const td_firstname1 = document.createElement("td");
     td_firstname1.innerHTML = pers.firstname1;
     tr_body.appendChild(td_firstname1);
-    if(pers.firstname2 === undefined){
+  
+    if (pers.firstname2 === undefined) {
         td_firstname1.colSpan = 2;
-    }else{
+    } else {
         const td_firstname2 = document.createElement("td");
         td_firstname2.innerHTML = pers.firstname2;
         tr_body.appendChild(td_firstname2);
     }
+    tr_body.addEventListener("click", function (e) {
+        console.log("clicked");
+        const a = tbody.querySelector(".selected")
+        if (a != undefined) {
+            a.classList.remove("selected")
+        }
+        e.currentTarget.classList.add("selected")
+    });
+    
+    const td_hazas = document.createElement("td")
+    td_hazas.innerHTML = pers.married;
+    tr_body.appendChild(td_hazas);
+    const td_pet = document.createElement("td")
+    td_pet.innerHTML = pers.pet;
+    tr_body.appendChild(td_pet);
+    
+    if(pers.married === true){
+    td_hazas.innerHTML="igen"
+        
+    }
+    else{
+     td_hazas.innerHTML="nem"
+    }
+
 }
- 
- 
