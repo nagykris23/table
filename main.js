69b109d9c9cffa180e6cd66fbb88128f8ccb9c5e
nagykris23 <nagykris68@gmail.com>
2024-10-04 13:56:07 +0200

    Updated js

--- main.js
+++ main.js
@@ -19,28 +19,41 @@ const array = [
         lastname: 'Horváth'
     },
 ]
-const table = document.createElement('table');
-const thead = document.createElement('thead');
-const tr= document.createElement('tr');
-const th_firstname= document.createElement('th');
-const th_lastname= document.createElement('th');
+ 
+ 
+const table = document.createElement("table");
+const thead = document.createElement("thead");
+const tr = document.createElement("tr");
+const th_lastname = document.createElement("th");
+const th_firstname = document.createElement("th");
 const tbody = document.createElement("tbody");
-
+ 
 document.body.appendChild(table);
 table.appendChild(thead);
 thead.appendChild(tr);
 tr.appendChild(th_lastname);
-th_lastname.innerHTML = "keresztnév"
 tr.appendChild(th_firstname);
-th_firstname.innerHTML = "vezetéknév"
-th_lastname.colSpan=2;
 table.appendChild(tbody);
-
+ 
+th_lastname.innerHTML = "Vezeténév";
+th_firstname.innerHTML = "Keresztnév";
+th_firstname.colSpan = 2;
+ 
 for(let pers of array){
-    const tr_body= document.createElement('tr');
+    const tr_body = document.createElement("tr");
     tbody.appendChild(tr_body);
-    const td_firstname= document.createElement('td');
-    td_firstname.innerHTML=pers.firstname1
-    const td_lastname= document.createElement('td');
-    td_lastname.innerHTML=pers.lastname
+    const td_lastname = document.createElement("td");
+    td_lastname.innerHTML = pers.lastname;
+    tr_body.appendChild(td_lastname);
+    const td_firstname1 = document.createElement("td");
+    td_firstname1.innerHTML = pers.firstname1;
+    tr_body.appendChild(td_firstname1);
+    if(pers.firstname2 === undefined){
+        td_firstname1.colSpan = 2;
+    }else{
+        const td_firstname2 = document.createElement("td");
+        td_firstname2.innerHTML = pers.firstname2;
+        tr_body.appendChild(td_firstname2);
+    }
 }
+ 
